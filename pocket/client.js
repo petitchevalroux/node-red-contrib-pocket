163911cb71cbc3b83b51170d3d66409c8638985b
Patrick Poulain <github@m41l.me>
2016-12-03 11:02:58 +0100

    Add doc to pocket client library

--- pocket/client.js
+++ pocket/client.js
@@ -1,9 +1,18 @@
 "use strict";
 var Promise = require("bluebird");
 
+/**
+ * Pocket API Client
+ * @param {string} consumerKey The consumer key for your application
+ * @returns {PocketClient}
+ */
 function PocketClient(consumerKey) {
     this.consumerKey = consumerKey;
 
+    /**
+     * Return request object
+     * @returns {Request}
+     */
     this.getRequest = function() {
         if (!this.defaultRequest) {
             var request = require("request");
@@ -18,6 +27,12 @@ function PocketClient(consumerKey) {
         return this.defaultRequest;
     };
 
+    /**
+     * Query path and resolve promise with parsed body as an object 
+     * @param {string} path
+     * @param {object} data
+     * @returns {Promise}
+     */
     this.query = function(path, data) {
         var self = this;
         return new Promise(function(resolve, reject) {
@@ -46,6 +61,15 @@ function PocketClient(consumerKey) {
         });
     };
 
+    /**
+     * Get request token from the API
+     * doc: https://getpocket.com/developer/docs/authentication
+     * @param {string} redirectUri The URL to be called when the authorization
+     * process has been completed.
+     * @param {string} state optional A string of metadata used by your app.
+     * This string will be returned in all subsequent authentication responses.
+     * @returns {Promise}
+     */
     this.getRequestToken = function(redirectUri, state) {
         var params = {
             "consumer_key": this.consumerKey,
@@ -60,6 +84,12 @@ function PocketClient(consumerKey) {
             });
     };
 
+    /**
+     * Get an access token from a request token
+     * doc: https://getpocket.com/developer/docs/authentication
+     * @param {string} requestToken request token
+     * @returns {Promise}
+     */
     this.getAccessToken = function(requestToken) {
         return this.query(
                 "oauth/authorize", {
@@ -72,6 +102,22 @@ function PocketClient(consumerKey) {
             });
     };
 
+    /**
+     * Add an item to pocket
+     * doc: https://getpocket.com/developer/docs/v3/add
+     * @param {string} accessToken
+     * @param {string} url
+     * @param {string} title (optional) This can be included for cases where an
+     * item does not have a title, which is typical for image or PDF URLs.
+     * If Pocket detects a title from the content of the page, this parameter
+     * will be ignored
+     * @param {string} tags (optional) A comma-separated list of tags to apply
+     * to the item
+     * @param {string} tweetId (optional) If you are adding Pocket support to a
+     * Twitter client, please send along a reference to the tweet status id.
+     * This allows Pocket to show the original tweet alongside the article.
+     * @returns {Promise}
+     */
     this.add = function(accessToken, url, title, tags, tweetId) {
         var params = {
             "consumer_key": this.consumerKey,
@@ -93,6 +139,14 @@ function PocketClient(consumerKey) {
             });
     };
 
+    /**
+     * Get url where redirect user to perform Pocket authentication
+     * doc: https://getpocket.com/developer/docs/authentication
+     * @param {string} requestToken request token
+     * @param {string} redirectUri the redirect_uri is the URL to be called when
+     * the user has completed the authorization within Pocket.
+     * @returns {String}
+     */
     this.getRedirectUrl = function(requestToken, redirectUri) {
         return "https://getpocket.com/auth/authorize?request_token=" +
             encodeURIComponent(requestToken) +
