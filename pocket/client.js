0d297ae42a7d83fe37244a796c4e744eca8f3419
Patrick Poulain <github@m41l.me>
2016-12-06 22:43:33 +0100

    Create retreive function for pocket client

--- pocket/client.js
+++ pocket/client.js
@@ -153,6 +153,87 @@ function PocketClient(consumerKey) {
             "&redirect_uri=" +
             encodeURIComponent(redirectUri);
     };
+
+    /**
+     * Retrieving a User's Pocket Data
+     * doc: https://getpocket.com/developer/docs/v3/retrieve
+     * @param {string} accessToken
+     * @param {object} options (optional) object containing options valid values are
+     *
+     * options.state {string} state (optional) (unread|archive|all)
+     *      unread = only return unread items (default)
+     *      archive = only return archived items
+     *      all = return both unread and archived items
+     * options.favorite {string} favorite (optional)
+     *      "0" = only return un-favorited items
+     *      "1" = only return favorited items
+     * options.tag {string} tag (optional)
+     *      tag_name = only return items tagged with tag_name
+     *      _untagged_ = only return untagged items
+     * options.contentType {string} contentType (optional) (article|video|image)
+     *      article = only return articles
+     *      video = only return videos or articles with embedded videos
+     *      image = only return images
+     * options.sort {string} sort (optional) (newest|oldest|title|site)
+     *      newest = return items in order of newest to oldest
+     *      oldest = return items in order of oldest to newest
+     *      title = return items in order of title alphabetically
+     *      site = return items in order of url alphabetically
+     * options.detailType {string} detailType (optional) (simple|complete)
+     *      simple = only return the titles and urls of each item
+     *      complete = return all data about each item, including tags, images, authors, videos and more
+     * options.search {string} (optional) search Only return items whose title or url contain the search string
+     * options.domain {string} (optional) domain Only return items from a particular domain
+     * options.since {int} since (optional) Only return items modified since the given since unix timestamp
+     * options.count {int} count (optional) Only return count number of items
+     * options.offset {int} offset (optional) Used only with count; start returning from offset position of results
+     * @returns {Promise}
+     */
+    this.retrieve = function(accessToken, options) {
+        var params = options || {};
+        params.consumer_key = this.consumerKey;
+        params.access_token = accessToken;
+        var self = this;
+        var p = new Promise(function(resolve, reject) {
+            self.query("get", params)
+                .then(function(response) {
+                    if (!response.list) {
+                        reject(new Error("Invalid response " +
+                            JSON.stringify({
+                                "response": response
+                            })));
+                    } else {
+                        var items = [];
+                        Object.getOwnPropertyNames(response.list)
+                            .forEach(function(name) {
+                                items.push(response.list[
+                                    name]);
+                            });
+                        // Sort items by sort_id
+                        items.sort(function(a, b) {
+                            if (typeof(a.sort_id) ===
+                                "undefined" ||
+                                typeof(b.sort_id) ===
+                                "undefined" ||
+                                a.sort_id === b.sort_id
+                            ) {
+                                return 0;
+                            } else if (a.sort_id < b.sort_id) {
+                                return -1;
+                            } else {
+                                return 1;
+                            }
+                        });
+                        resolve(items);
+                    }
+                    return response;
+                })
+                .catch(function(error) {
+                    reject(error);
+                });
+        });
+        return p;
+    };
 }
 
 module.exports = PocketClient;
