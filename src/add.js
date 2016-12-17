fcd3e8c31e08599608ad65e17a1d131b765c8da1
Patrick Poulain <github@m41l.me>
2016-12-17 11:02:12 +0100

    Rename pocket out to pocket add

--- src/add.js
+++ src/add.js
@@ -4,7 +4,7 @@ var PocketClient = require(path.join(__dirname, "client"));
 var credentialsHelper = require(path.join(__dirname, "credentials-helper"));
 
 module.exports = function(RED) {
-    RED.nodes.registerType("pocket out", function(config) {
+    RED.nodes.registerType("pocket add", function(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.pocketCredentials = RED.nodes.getCredentials(config.pocketCredentials);
